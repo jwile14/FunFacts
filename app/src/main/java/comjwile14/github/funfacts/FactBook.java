a73896f07e189b6745b5614af9efb14e845294df
Jenner <wilejd@rose-hulman.edu>
2015-06-03 14:32:35 -0400

    refactoring

---
+++ app/src/main/java/comjwile14/github/funfacts/FactBook.java
@@ -0,0 +1,38 @@
+package comjwile14.github.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by wilejd on 6/3/2015.
+ */
+public class FactBook {
+
+    // Member variables (properties about the object)
+
+    // Methods (abilities: things the object can do)
+    public String getFact() {
+
+        String[] facts = {
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver.",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the Sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water.",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the earth when the Great Pyramid was being built.",};
+
+        // The button was clicked, so update the fact label with a new fact
+        String fact = "";
+
+        // Randomly select a fact
+        Random randomGenerator = new Random(); // Constructs a new Random number generator
+        int randomNumber = randomGenerator.nextInt(facts.length);
+
+        fact = facts[randomNumber];
+
+        return fact;
+    }
+}
--- app/src/main/java/comjwile14/github/funfacts/FunFactsActivity.java
+++ app/src/main/java/comjwile14/github/funfacts/FunFactsActivity.java
@@ -1,9 +1,10 @@
 package comjwile14.github.funfacts;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class FunFactsActivity extends ActionBarActivity {
@@ -12,27 +13,19 @@ public class FunFactsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
+        // Declare our View variables and assign them the Views from the layout files
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+                // Update the label with our dynamic fact
+                factLabel.setText("");
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
